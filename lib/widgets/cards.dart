b4717c772f4b90ee52752d1c4909dbe04982f93e
mustafayen <mustafayeng@gmail.com>
2024-02-19 21:14:52 +0300

    3

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -82,11 +82,22 @@ Card markaCard(
                     height: 50,
                     color: Colors.blue,
                     child: Center(
-                      child: Image.asset(
-                        logoUrl,
-                        width: 50,
-                        height: 50,
-                        fit: BoxFit.fill,
+                      child: FutureBuilder<String>(
+                        future: getImageUrl(logoUrl), // Resim URL'sini getir
+                        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            // Veri yüklenene kadar bekleyen durum
+                            return CircularProgressIndicator();
+                          } else {
+                            if (snapshot.hasError) {
+                              // Hata durumu
+                              return Text('Resim yüklenirken bir hata oluştu: ${snapshot.error}');
+                            } else {
+                              // Veri başarıyla yüklendiği durum
+                              return snapshot.data != null ? Image.network(snapshot.data!) : SizedBox(); // Resim mevcutsa göster, değilse boş bir SizedBox göster
+                            }
+                          }
+                        },
                       ),
                     ),
                   ),
